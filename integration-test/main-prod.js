29844650e5e63bb71718c925700c422c5d9a7afb
Torgeir Helgevold <torgeirh@hotmail.com>
2019-11-29 13:39:55 -0500

    Add test application

---
+++ integration-test/main-prod.js
@@ -0,0 +1,7 @@
+import Component from "./ProdApp.svelte";
+
+const app = new Component({
+  target: document.getElementById("prod")
+});
+
+export default app;
--- integration-test/main.js
+++
@@ -1,7 +0,0 @@
-import Component from "./App.svelte";
-
-const app = new Component({
-  target: document.body
-});
-
-export default app;
